94b942526a6729f55866340ba249be19e8c7a939
Gbadamosi Lateef Oluware <85267407+Liberty-slug@users.noreply.github.com>
2022-10-27 16:18:35 +0100

    Create a basic webpage with frontend technologies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './profile.png'
+import github from './Icon.png'
+import slack from './Slack_icon.png'
+import vector from './Vector.png'
+import I4G from './I4G.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="linktree">
+         <div className="profile-section">
+            <img src={logo} alt="" id="profile__img"/>
+            <h2 id="twitter">Iamharyobahmi</h2>
+            <h2 id="slack" hidden>Iamharyobahmi</h2>
+         </div>
+         <div className="link-section">
+            <button><a href="https://twitter.com/iamharyobahmi" rel="Twitter link">Twitter Link</a></button>
+            <button><a href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a></button>
+            <button><a href="http://books.zuri.team" id="books">Zuri Books</a></button>
+            <button><a href="https://books.zuri.team/python-for-beginners?ref_id=LibertyTech" id="book__python">Python Books</a></button>
+            <button><a href="https://background.zuri.team" id="pitch">Background Check for Code</a></button>
+            <button><a href="https://books.zuri.team/design-rules" id="book__design">Design Books</a></button>
+         </div>
+         <div className="social-section">
+            <img src={slack} alt="" height="30"/>
+            <img src={github} alt="" height="30"/>
+         </div>
+         <div className="footer">
+            <div className="container">
+              <img src={vector} alt="" height="30" />
+              <p>HNG Internship 9 Frontend Task</p>
+              <img src={I4G} alt="" height="30"/>
+            </div>
+         </div>
+      </div>
+    </>
   );
 }
 
