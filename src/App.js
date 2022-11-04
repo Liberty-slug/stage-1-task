cedad9c7c3a8ae729ca735ae608ba3fa5298c3a5
Gbadamosi Lateef Oluware <85267407+Liberty-slug@users.noreply.github.com>
2022-11-04 17:16:13 +0100

    Creating the contact page with necessary styles

--- src/App.js
+++ src/App.js
@@ -1,64 +1,18 @@
 import React from 'react';
-import './App.css';
-import logo from './profile.png'
-import github from './Icon.png'
-import slack from './Slack_icon.png'
-import vector from './Vector.png'
-import I4G from './I4G.png'
-import share from './Icon-share.png'
-import dot from './Icon-dot.png'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
-function App() {
+import Home from './home/home';
+import Contact from './contact/contact';
+
+function App () {
   return (
-    <>
-      <div className="linktree">
-
-        {/* Share Button Start */}
-        <div className="share-button">
-          <img src={share} alt="" className='share'/>
-          <img src={dot} alt="" className='dot'/>
-        </div>
-        {/* Share Button end */}
-
-        {/* Profile Section start */}
-         <div className="profile-section">
-            <img src={logo} alt="" id="profile__img"/>
-            <h2 id="twitter">Iamharyobahmi</h2>
-            <h2 id="slack" hidden>LibertyTech</h2>
-         </div>
-         {/* Profile Section end */}
-
-          {/* Link section Start */}
-         <div className="link-section">
-            <a href="https://twitter.com/iamharyobahmi" rel="Twitter link">Twitter Link</a>
-            <a href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a>
-            <a href="http://books.zuri.team" id="books" title="This is where you find books about design and coding">Zuri Books</a>
-            <a href="https://books.zuri.team/python-for-beginners?ref_id=LibertyTech" id="book__python" title='Buy my Python book for beginners'>Python Books</a>
-            <a href="https://background.zuri.team" id="pitch" title='Do a background check on a coder here'>Background Check for Code</a>
-            <a href="https://books.zuri.team/design-rules" id="book__design" title='Get a free design book offered by Zuri.'>Design Books</a>
-         </div>
-         {/* Link section ends */}
-
-
-         {/* Social Section Start */}
-         <div className="social-section">
-            <img src={slack} alt="" height="30"/>
-            <img src={github} alt="" height="30"/>
-         </div>
-         {/* Social Section end */}
-
-         {/* Footer Section start */}
-         <div className="footer">
-            <div className="container">
-              <img src={vector} alt="" height="30" />
-              <p>HNG Internship 9 Frontend Task</p>
-              <img src={I4G} alt="" height="30"/>
-            </div>
-         </div>
-         {/* Footer Section end */}
-
-      </div>
-    </>
+      <Router>
+          <Routes>  
+          <Route path="/" element={<Home />} />    
+          <Route path="/contact" element={<Contact />} />    
+          </Routes>
+      </Router>
+    
   );
 }
 
---
+++ src/contact/contact.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import '../App.css';
+import vector from '../Vector.png'
+import I4G from '../I4G.png'
+
+const Contact = () => {
+    return(
+        <div className="contact-page">
+            <div className="containeer">
+                <div className="content">
+                    <div className="content-detail">
+                        <h2 className='heading'>Contact Me</h2>
+                        <p className="supporting-text">Hi there, contact me to ask me about anything you have in mind</p>
+                    </div>
+                    <div className="form">
+                        <div className="form-field">
+                            <div className="name-row">
+                                <div className="first-name">
+                                    <label htmlFor="firstname">First Name</label>
+                                    <input type="text" placeholder='Enter your first name' id="first_name"/>
+                                </div>
+                                <div className="last-name">
+                                    <label htmlFor="lastname">Last Name</label>
+                                    <input type="text" placeholder='Enter your last name' id="last_name"/>
+                                </div>
+                            </div>
+                            <div className="email">
+                                <label htmlFor="email">Email</label>
+                                <input type="text" placeholder='yourname@email.com' id="email"/>
+                            </div>
+                            <div className="textarea">
+                                <label htmlFor="message">Message</label>
+                                <textarea type="text" placeholder="Send me a message and I'll reply you as soon as possible" id="message"/>
+                            </div>
+                            <div className="checkbox">
+                                <input type="checkbox"/>
+                                <p>You agree to providing your data to (name) who may contact you.</p>
+                            </div>
+                        </div>
+                        <button id='btn_submit'>Send message</button>
+                    </div>
+                </div>
+            </div>
+                    {/* Footer Section start */}
+                    <div className="footer">
+                        <div className="container">
+                        <img src={vector} alt="" height="30" />
+                        <p>HNG Internship 9 Frontend Task</p>
+                        <img src={I4G} alt="" height="30"/>
+                        </div>
+                    </div>
+                    {/* Footer Section end */}
+        </div>
+    )
+}
+export default Contact;
