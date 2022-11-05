c5eca045f7b999b125ac1fb9cf0e816b9a009bd5
Gbadamosi Lateef Oluware <85267407+Liberty-slug@users.noreply.github.com>
2022-11-05 11:08:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App () {
   return (
       <Router>
           <Routes>  
-          <Route path="/" element={<Home />} />    
+          <Route index element={<Home />} />    
           <Route path="/contact" element={<Contact />} />    
           </Routes>
       </Router>
